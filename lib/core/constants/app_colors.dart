786af037ddfd302eb3268bca3faf9e7d95e83823
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-05-10 00:29:37 +0300

    Initial commit

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
-mixin AppColors {
+abstract class AppColors {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
