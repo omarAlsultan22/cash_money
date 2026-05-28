08f9141ef8cca48ae7295e5d63e5dc65bfdf2bda
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-05-28 11:15:00 +0300

    Initial commit

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -2,6 +2,8 @@ mixin class AppSizes {
   static const double none = 0.0;
   static const double medium = 50.0;
   static const double radius = 12.0;
+
+  //fonts
   static const double fontSize_16 = 16.0;
   static const double fontSize_18 = 18.0;
   static const double fontSize_24 = 24.0;
