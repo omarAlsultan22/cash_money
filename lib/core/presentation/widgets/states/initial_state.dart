89427d469411b35fb42a2d0e2470bbfc2054da9d
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-05-31 17:20:26 +0300

    Initial commit

--- lib/core/presentation/widgets/states/initial_state.dart
+++ lib/core/presentation/widgets/states/initial_state.dart
@@ -1,5 +1,3 @@
-import 'package:cash_money/core/constants/app_colors.dart';
-import 'package:cash_money/core/constants/app_sizes.dart';
 import 'package:flutter/material.dart';
 
 
@@ -8,25 +6,6 @@ class InitialStateWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.menu,
-            size: 100.0,
-            color: Color(0xFFE0E0E0),
-          ),
-          Text(
-            'No Tasks Yet, Please Add Some Tasks',
-            style: TextStyle(
-              fontSize: AppSizes.fontSize_16,
-              fontWeight: FontWeight.bold,
-              color: AppColors.grey500,
-            ),
-          ),
-        ],
-      ),
-    );
+    return const SizedBox();
   }
 }
